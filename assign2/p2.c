122932bbeadc03557f77b2c8d8b07bdbe126793c
Kumar Ayush <cheekujodhpur@gmail.com>
2017-09-24 13:08:04 +0530

    not working signal, still to read from file

--- assign2/p2.c
+++ assign2/p2.c
@@ -2,45 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_THREADS 3
-#define COUNT_LIMIT 12
+#define NUM_THREADS 10
+#define COUNT_LIMIT 50
 
-int count = 0;
+int count[20];
+int total_count = 0;
 
 pthread_mutex_t count_mutex;
 pthread_cond_t count_threshold_cv;
 
+void *inc_count(void *file) {
 
-void *inc_count(void *t) {
-    int i;
-    long my_id = (long) t;
-    int personalLock = 1;
+    FILE *filename = (FILE*)file;
+    int personal_lock = 1;
 
-    while(personalLock){
+    while(personal_lock) {
         pthread_mutex_lock(&count_mutex);
-        printf("This is thread %i modifying count from %i\n", my_id, count);
-        count++;
+        printf("This is thread modifying count from %i\n", total_count);
+        // TODO: Actually read from file and update coutns
+        total_count++;
 
-        if(count >= COUNT_LIMIT) {
+        if(!(total_count%COUNT_LIMIT)) {
             pthread_cond_signal(&count_threshold_cv);
-            personalLock = 0;
         }
 
         pthread_mutex_unlock(&count_mutex);
-
         sleep(1);
     }
 
     pthread_exit(NULL);
 }
 
-void *watch_count(void *t) {
-    long my_id = (long) t;
+void *watch_count(void *dummy) {
 
     pthread_mutex_lock(&count_mutex);
-    while(count < COUNT_LIMIT) {
+    while(total_count%COUNT_LIMIT) {
         pthread_cond_wait(&count_threshold_cv, &count_mutex);
-        printf("Signal received at count %i\n", count);
+
+       // for(int i = 0;i<4;i++) {
+       //     for(int j = 0;j<5;j++)
+       //         printf("%i ", count[i]);
+       //     printf("\b\t");
+       // }
+        printf("Signal received at count %i\n", total_count);
     }
     pthread_mutex_unlock(&count_mutex);
     pthread_exit(NULL);
@@ -49,8 +53,25 @@ void *watch_count(void *t) {
 
 int main(int argc, char *argv[]) {
 
-    long t1=1, t2=2, t3=3; // IDs?
-    pthread_t threads[NUM_THREADS];
+    // Initialize counts
+    for( int i = 0;i<20;++i ) {
+        count[i] = 0;
+    }
+
+    char filename[NUM_THREADS][80];
+
+    FILE *filename_list;
+    filename_list = fopen("filename_list.txt", "r");
+
+    FILE *filenames[NUM_THREADS];
+    
+    int iter_f = 0;
+    while(fgets(filename[iter_f], sizeof(filename[iter_f]), filename_list) && iter_f < 10) {
+        filenames[iter_f] = fopen(filename[iter_f], "r");
+        iter_f++;
+    }
+
+    pthread_t threads[NUM_THREADS+1];
     pthread_attr_t attr;
 
     // Initialize mutex and conditional variable
@@ -60,15 +81,19 @@ int main(int argc, char *argv[]) {
     // joined state
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_create(&threads[0], &attr, watch_count, (void *)t1);
-    pthread_create(&threads[1], &attr, inc_count, (void *)t2);
-    pthread_create(&threads[2], &attr, inc_count, (void *)t3);
+
+    int dummy;
+    pthread_create(&threads[0], &attr, watch_count, (void *)dummy);
+
+    for(int iter = 0;iter < NUM_THREADS; ++iter) {
+        pthread_create(&threads[iter+1], &attr, inc_count, (void *)filenames[iter]);
+    }
 
     // Wait for all threads to complete
     for(int i = 0;i<NUM_THREADS;++i) {
         pthread_join(threads[i], NULL);
     }
-    printf("Final value of count is %i\n", count);
+    // printf("Final value of count is %i\n", count);
 
     // Clean up
     pthread_attr_destroy(&attr);
