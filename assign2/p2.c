75100d9c906afe4635211694a3d4d351f381213c
Kumar Ayush <cheekujodhpur@gmail.com>
2017-09-24 22:12:38 +0530

    p2 some progress but still failing

--- assign2/p2.c
+++ assign2/p2.c
@@ -7,28 +7,49 @@
 int count[20];
 int total_count = 0;
 int COUNT_LIMIT = 50;
+int thread_count = 0;
 
 pthread_mutex_t count_mutex;
 pthread_cond_t count_threshold_cv;
 
-void *inc_count(void *file) {
+void *inc_count(void *_file) {
 
-    FILE *filename = (FILE*)file;
-    int personal_lock = 1;
-
-    while(personal_lock) {
-        pthread_mutex_lock(&count_mutex);
-        printf("This is thread modifying count from %i\n", total_count);
-        // TODO: Actually read from file and update coutns
-        total_count++;
-
-        if(total_count==COUNT_LIMIT) {
-            pthread_cond_signal(&count_threshold_cv);
-        }
-
-        pthread_mutex_unlock(&count_mutex);
-        sleep(1);
+    //FILE *file = fopen((char*)_file,"r");
+    // TODO: This file reading failing for some reason,
+    // using a static name for now
+    FILE *file = fopen("input1.txt","r");
+    if(!file) {
+        printf("Error opening file!\n");
+        pthread_exit(NULL);
     }
+    int personal_lock = 1;
+    int party, local_count;
+
+    pthread_mutex_lock(&count_mutex);
+    //printf("This is thread modifying count from %i\n", total_count);
+    // TODO: Actually read from file and update coutns
+    char sentence[80];
+    fgets(sentence, sizeof(sentence), file);
+    while(fgets(sentence, sizeof(sentence), file)) {
+        if(sentence[0]=='\n') {
+            // get the district number and let it go
+            fgets(sentence, sizeof(sentence), file);
+            continue;
+        }
+        sscanf(sentence, "%i%i", &party, &local_count);
+        count[party-1] += local_count;
+        total_count += local_count;
+    }
+
+    if(total_count>=COUNT_LIMIT) {
+        pthread_cond_signal(&count_threshold_cv);
+    }
+
+    fclose(file);
+    thread_count++;
+
+    pthread_mutex_unlock(&count_mutex);
+    sleep(1);
 
     pthread_exit(NULL);
 }
@@ -36,16 +57,18 @@ void *inc_count(void *file) {
 void *watch_count(void *dummy) {
 
     pthread_mutex_lock(&count_mutex);
-    while(total_count<COUNT_LIMIT) {
+    while(thread_count<10) {
         pthread_cond_wait(&count_threshold_cv, &count_mutex);
 
-       // for(int i = 0;i<4;i++) {
-       //     for(int j = 0;j<5;j++)
-       //         printf("%i ", count[i]);
-       //     printf("\b\t");
-       // }
-        printf("Signal received at count %i\n", total_count);
+        for(int i = 0;i<4;i++) {
+            for(int j = 0;j<5;j++)
+                printf("%i ", count[5*i+j]);
+            printf("\b\t");
+        }
+        printf("\n");
         COUNT_LIMIT += 50;
+        // TODO: this loop not working properly, erratic behavior
+        // if(thread_count>=10)break;
     }
     pthread_mutex_unlock(&count_mutex);
     pthread_exit(NULL);
@@ -64,11 +87,9 @@ int main(int argc, char *argv[]) {
     FILE *filename_list;
     filename_list = fopen("filename_list.txt", "r");
 
-    FILE *filenames[NUM_THREADS];
     
     int iter_f = 0;
     while(fgets(filename[iter_f], sizeof(filename[iter_f]), filename_list) && iter_f < 10) {
-        filenames[iter_f] = fopen(filename[iter_f], "r");
         iter_f++;
     }
 
@@ -87,7 +108,7 @@ int main(int argc, char *argv[]) {
     pthread_create(&threads[0], &attr, watch_count, (void *)dummy);
 
     for(int iter = 0;iter < NUM_THREADS; ++iter) {
-        pthread_create(&threads[iter+1], &attr, inc_count, (void *)filenames[iter]);
+        pthread_create(&threads[iter+1], &attr, inc_count, (void *)filename[iter]);
     }
 
     // Wait for all threads to complete
