598fe6e4ad6fdd693bdd798139d7cb270a5bd7a7
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-09-30 02:19:24 +0530

    Switched to cond vars, code working well

--- assign2/generate.py
+++ assign2/generate.py
@@ -1,13 +1,21 @@
 import numpy as np
 
-districts = 5
-entries = 100000
+districts = 20
+constituencies = 5
+files = 10
+parties = 20
 
-for j in range(10):
+for j in range(files):
     output = ""
     for k in range(districts):
-        output += str(np.random.randint(1, 1001)) + "\n"
-        output += '\n'.join(["%s %s" % (np.random.randint(1, 21), np.random.randint(1, 6)) for i in range(entries)])
-        output += '\n\n'
-    with open("%d.txt" % j, 'w') as f:
+        output += str(j * files + k) + "\n"
+        winners = [0] * parties
+        for i in range(constituencies):
+            winners[np.random.randint(0, parties)] += 1
+        for i in range(parties):
+            if winners[i] == 0:
+                continue
+            output += str(i + 1) + " " + str(winners[i]) + "\n"
+        output += '\n'
+    with open("%d.txt" % (j + 1), 'w') as f:
         f.write(output)
--- assign2/p2.c
+++ assign2/p2.c
@@ -4,20 +4,21 @@
 #include <stdlib.h>
 
 #define NUM_THREADS 10
+#define TOTAL 1000
 
 int count[20];
 int total_count = 0;
 int COUNT_LIMIT = 50;
 
 pthread_mutex_t count_mutex;
+pthread_cond_t count_threshold_cv, count_wait;
 
 void *inc_count(void *_file) {
-
     char filename[80];
     sscanf((char*)_file, "%s", filename);
     FILE *file = fopen(filename,"r");
     if(!file) {
-        printf("Error opening file!\n");
+        printf("Error opening file %s!\n", filename);
         pthread_exit(NULL);
     }
     int party, local_count;
@@ -36,22 +37,49 @@ void *inc_count(void *_file) {
         // we're using a common mutex for all parties to ensure that `count[]` updates
         // synchronously. This will ensure `total_count` outputs at the correct timesteps
         pthread_mutex_lock(&count_mutex);
-        count[party-1] += local_count;
+        // The `while` loop is essential since a number of threads are waiting on this condition
+        // Votes will be counted ONLY when its sure that total_count < COUNT_LIMIT
+        while(total_count >= COUNT_LIMIT) {
+            pthread_cond_wait(&count_wait, &count_mutex);
+        }
+        count[party - 1] += local_count;
         total_count += local_count;
-        if(total_count>=COUNT_LIMIT) {
-            for(int i = 0;i<4;i++) {
-                for(int j = 0;j<5;j++)
-                    printf("%i ", count[5*i+j]);
-                printf("\b\t");
-            }
-            printf("\n");
-            COUNT_LIMIT += 50;
+        if(total_count >= COUNT_LIMIT) {
+            // Carry out printing process in `wait_count` thread. As long as this condition
+            // remains true, counter threads are blocked until a print takes place
+            pthread_cond_signal(&count_threshold_cv);
         }
         pthread_mutex_unlock(&count_mutex);
     }
 
     fclose(file);
+    pthread_exit(NULL);
+}
 
+
+void* wait_count(void* t) {
+    pthread_mutex_lock(&count_mutex);
+    while(total_count < TOTAL) {
+        // Thread blocked till enough votes counted for printing
+        pthread_cond_wait(&count_threshold_cv, &count_mutex);
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                sum += count[5 * i + j];
+                printf("%i ", count[5 * i + j]);
+            }
+            printf("\b\t");
+        }
+        // Uncomment this to prove code's correctness!
+        // printf(" - %i\n", sum);
+        printf("\n");
+
+        COUNT_LIMIT += 50;
+        // COUNT_LIMIT has been raised and results have been printed
+        // vote counting can continue
+        pthread_cond_broadcast(&count_wait);
+    }
+    pthread_mutex_unlock(&count_mutex);
     pthread_exit(NULL);
 }
 
@@ -77,16 +105,20 @@ int main(int argc, char *argv[]) {
         iter_f++;
     }
 
+    pthread_t wait_thread;
     pthread_t threads[NUM_THREADS];
     pthread_attr_t attr;
 
     // Initialize mutex and conditional variable
     pthread_mutex_init(&count_mutex, NULL);
+    pthread_cond_init(&count_wait, NULL);
+    pthread_cond_init(&count_wait, NULL);
 
     // joined state
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
+    pthread_create(&wait_thread, &attr, wait_count, NULL);
     for(int iter = 0;iter < NUM_THREADS; ++iter) {
         pthread_create(&threads[iter], &attr, inc_count, (void *)filename[iter]);
     }
@@ -95,6 +127,7 @@ int main(int argc, char *argv[]) {
     for(int i = 0;i<NUM_THREADS;++i) {
         pthread_join(threads[i], NULL);
     }
+    pthread_join(wait_thread, NULL);
 
     // Printing final values
     for(int i = 0;i<4;i++) {
@@ -107,6 +140,8 @@ int main(int argc, char *argv[]) {
     // Clean up
     pthread_attr_destroy(&attr);
     pthread_mutex_destroy(&count_mutex);
+    pthread_cond_destroy(&count_threshold_cv);
+    pthread_cond_destroy(&count_wait);
     pthread_exit(NULL);
 
     return 0;
